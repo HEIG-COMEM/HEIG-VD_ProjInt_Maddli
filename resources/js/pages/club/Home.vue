b85ac905ed61b7492fdc2a8be062378319cf85e2
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-13 14:09:58 +0100

    feat(club): add club routes and pages for authenticated and guest users

---
+++ resources/js/pages/club/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Club"></Head>
+    <h1>Club - Home</h1>
+</template>
---
+++ routes/club.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::prefix('club')->group(function () {
+
+    // Globally accessible route
+    Route::get('/infos', function () {
+        return Inertia::render('club/Infos');
+    })->name('club.infos');
+
+    // Only accessible to guests
+    Route::middleware('guest')->group(function () {
+        // Redirect to the club infos page if the user is a guest
+        Route::get('/', function () {
+            return redirect()->route('club.infos');
+        });
+    });
+
+    // Only accessible to authenticated users
+    Route::middleware('auth')->group(function () {
+        Route::get('/', function () {
+            return Inertia::render('club/Home');
+        })->name('club.home');
+    });
+});
