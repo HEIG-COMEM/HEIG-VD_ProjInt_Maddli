73664efcc8c75bf9274e05f19990337097512f93
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-25 22:21:37 +0100

    feat(seeders): add CoachingSeeder to create users with coaching licences and assign them to club leagues

--- database/seeders/CoachingSeeder.php
+++ database/seeders/CoachingSeeder.php
@@ -4,9 +4,17 @@ namespace Database\Seeders;
 
 use App\Models\ClubLeague;
 use App\Models\Coaching;
+use App\Models\Licence;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
+/**
+ * Class CoachingSeeder
+ *
+ * Create users assign them a coaching licence and assign them to a club league.
+ * This seeder handle the creation of actual coaching data.
+ */
 class CoachingSeeder extends Seeder
 {
     /**
@@ -14,9 +22,17 @@ class CoachingSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(100)->create();
+
         $users = User::all();
+        $licences = Licence::all();
 
         foreach ($users as $user) {
+
+            // Assign a random licence to the user. Each user should have a licence for coaching.
+            $user->licence_id = $licences->random()->id;
+            $user->save();
+
             $clubLeague = ClubLeague::inRandomOrder()->first();
             Coaching::firstOrCreate([
                 'club_league_id' => $clubLeague->id,
