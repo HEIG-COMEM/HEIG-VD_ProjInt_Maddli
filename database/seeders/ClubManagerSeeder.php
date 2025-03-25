3ea1294a8c917c1ba4b69fcb5e1ea86ead023dde
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-25 22:21:22 +0100

    feat(seeders): create ClubManagerSeeder to assign club managers to clubs

--- database/seeders/ClubManagerSeeder.php
+++ database/seeders/ClubManagerSeeder.php
@@ -4,8 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Club;
+use App\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
+/**
+ * Class ClubManagerSeeder
+ *
+ * Create Users with the role of club_manager and assign them to clubs.
+ */
 class ClubManagerSeeder extends Seeder
 {
     /**
@@ -14,11 +21,13 @@ class ClubManagerSeeder extends Seeder
     public function run(): void
     {
         $clubs = Club::inRandomOrder()->limit(10)->get();
+        $role = Role::where('name', 'club_manager')->first();
         foreach ($clubs as $club) {
             $user = User::firstOrCreate(
                 ['email' => strtolower(str_replace(' ', '_', $club->name)) . '@maddli.com'],
-                ['name' => $club->name . ' Manager', 'password' => bcrypt('manager'), 'birth_date' => now()->subYears(30)]
+                ['name' => $club->name . ' Manager', 'password' => Hash::make('manager'), 'birth_date' => now()->subYears(30)]
             );
+            $user->roles()->sync($role);
 
             $club->managers()->sync($user);
         }
