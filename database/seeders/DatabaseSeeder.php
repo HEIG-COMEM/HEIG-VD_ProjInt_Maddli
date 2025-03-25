63bc843830fab00e6cc2c25a7a7e4743d854835c
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-25 09:43:26 +0100

    chore(seeders): remove unused NationTableSeeder and LeagueTableSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleTableSeeder::class,
             LicenceTableSeeder::class,
-            // NationTableSeeder::class,
-            // LeagueTableSeeder::class,
             DataSeeder::class,
         ]);
 
