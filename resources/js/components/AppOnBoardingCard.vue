744522fd9a3cde34e976155d6a11a9377b6b0988
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-31 14:38:34 +0200

    feat(club): add onboarding cards and icons to the club home page

---
+++ resources/js/components/AppOnBoardingCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { Link } from '@inertiajs/vue3';
+import type { LucideIcon } from 'lucide-vue-next';
+import { defineProps, withDefaults } from 'vue';
+
+interface Props {
+    title: string;
+    href?: string;
+    icon?: LucideIcon;
+}
+withDefaults(defineProps<Props>(), {
+    title: '',
+    href: undefined,
+    icon: () => ({}) as LucideIcon,
+});
+</script>
+
+<template>
+    <Link v-if="href" :href>
+        <Card class="flex flex-col items-center justify-center bg-accent p-4 text-accent-foreground">
+            <CardContent>
+                <slot name="icon"></slot>
+            </CardContent>
+            <CardTitle>{{ title }}</CardTitle>
+        </Card>
+        <Card class="flex flex-col items-center justify-center bg-accent p-4 text-accent-foreground">
+            <CardContent>
+                <slot name="icon"></slot>
+            </CardContent>
+            <CardTitle>{{ title }}</CardTitle>
+        </Card>
+    </Link>
+    <Card v-else class="flex flex-col items-center justify-center bg-accent p-4 text-accent-foreground">
+        <CardContent>
+            <slot name="icon"></slot>
+        </CardContent>
+        <CardTitle>{{ title }}</CardTitle>
+    </Card>
+</template>
+
+<style scoped></style>
--- resources/js/pages/club/Home.vue
+++ resources/js/pages/club/Home.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import AppOnBoardingCard from '@/components/AppOnBoardingCard.vue';
+import { H1, H3 } from '@/components/typography/headings';
+import { Lead, P } from '@/components/typography/texts';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../../components/PlaceholderPattern.vue';
 
 defineProps<{
     user: any;
@@ -21,19 +23,43 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
+            <div>
+                <H1
+                    >Hey, <span class="text-primary">{{ user.name }}</span></H1
+                >
+                <Lead>Get started and get your questions answered!</Lead>
             </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                {{ user }}
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <AppOnBoardingCard title="Contact your federation">
+                    <template #icon>
+                        <img src="/assets/icons/Football_Suisse_federation.svg" alt="Talk to ASF" class="h-16" />
+                    </template>
+                </AppOnBoardingCard>
+                <AppOnBoardingCard title="Talk to ambassadors">
+                    <template #icon>
+                        <img src="/assets/icons/ambassadors.png" alt="Talk to ambassadors" class="h-16" />
+                    </template>
+                </AppOnBoardingCard>
+                <AppOnBoardingCard title="Talk to mentors">
+                    <template #icon>
+                        <img src="/assets/icons/coaches.png" alt="Talk to mentors" class="h-16" />
+                    </template>
+                </AppOnBoardingCard>
+            </div>
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 p-4 dark:border-sidebar-border md:min-h-min">
+                <H3>Why is it important to talk to each other</H3>
+                <P>
+                    Becoming a coach is not just about taking a course — it’s about understanding what it really means to lead, support, and grow with
+                    a team. That’s why connecting with others who have already gone through this journey is so important. By talking to ambassadors,
+                    you can hear real stories from active coaches who once stood where you are now. They can share practical tips, motivation, and
+                    honest advice that no textbook will give you.
+                </P>
+                <P>
+                    Mentors, on the other hand, are certified coaches with experience in higher categories. They are here to guide you through the
+                    next steps, help you navigate the training process, and support you as you grow your coaching identity. Whether you’re still
+                    hesitating or already preparing your registration, these conversations are your opportunity to ask questions, gain clarity, and
+                    feel supported by people who believe in you. Coaching starts with connection — and your journey starts here.
+                </P>
             </div>
         </div>
     </AppLayout>
