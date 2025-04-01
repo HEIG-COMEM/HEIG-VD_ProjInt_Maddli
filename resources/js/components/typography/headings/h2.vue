be2c7b72fc0cec8e626b5beb20486170a1ec6588
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-04-01 09:25:55 +0200

    style(typography): remove unnecessary border from h2 heading

--- resources/js/components/typography/headings/h2.vue
+++ resources/js/components/typography/headings/h2.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+    <h2 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         <slot></slot>
     </h2>
 </template>
