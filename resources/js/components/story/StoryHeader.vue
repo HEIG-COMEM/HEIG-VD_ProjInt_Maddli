06c4ef49c3e6eb3e412595fa9f5ea3c0c6879f8b
antoineuldry <antoine.uldry@heig-vd.ch>
2025-04-02 16:14:42 +0200

    fix(story): update Story component layout for better responsiveness and enhance StoryHeader with logo and chapter base

--- resources/js/components/story/StoryHeader.vue
+++ resources/js/components/story/StoryHeader.vue
@@ -4,7 +4,13 @@ import StoryProgress from './StoryProgress.vue';
 
 <template>
     <div>
-        <div class="h-[5dvh]">Header</div>
+        <div class="flex h-[10dvh] items-center justify-between">
+            <div class="ml-4">Logo</div>
+            <div class="mr-4 text-right">
+                <div>Chapter N</div>
+                <div>ChapterTitle</div>
+            </div>
+        </div>
         <StoryProgress />
     </div>
 </template>
