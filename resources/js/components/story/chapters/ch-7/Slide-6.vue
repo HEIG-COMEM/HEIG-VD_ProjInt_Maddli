4abbbed90b6c6ef0e7373a18086e2b35262017fa
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-04-03 15:18:02 +0200

    feat(story): add basic confing

--- resources/js/components/story/chapters/ch-7/Slide-6.vue
+++ resources/js/components/story/chapters/ch-7/Slide-6.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <section>
+    <section data-background-image="/assets/story/bg/8.png" data-auto-animate>
         <h1>Chapter 7 - Slide 6</h1>
     </section>
 </template>
