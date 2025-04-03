09f24767d7fba8eb8198d045155136301ac7075c
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-04-03 15:53:15 +0200

    feat(story): add Parent and TheCoach components to Chapter 7 slides

---
+++ resources/js/components/story/characters/Parent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useWindowSize } from '@vueuse/core';
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+    chNumber: {
+        type: Number,
+        required: true,
+    },
+    poseNumber: {
+        type: Number,
+        required: true,
+    },
+    scale: {
+        type: Number,
+        default: 1,
+    },
+});
+
+const { width } = useWindowSize();
+const adjustedScale = computed(() => (width.value <= 1280 ? props.scale / 2 : props.scale));
+</script>
+
+<template>
+    <img
+        :src="`/assets/story/ch-${props.chNumber}/parents/${props.poseNumber}.svg`"
+        alt="A Parent"
+        class="absolute bottom-0 left-0 object-contain"
+        :style="{ zIndex: 1000, transform: `scale(${adjustedScale}) translateX(-50%)` }"
+    />
+</template>
+
+<style scoped></style>
