6e258d93ba7251b3a3122e036ffe8ae40ffd94cd
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-04-04 15:30:08 +0200

    feat(story): enhance Slide 2, Slide 3, and Slide 7 with improved layouts and dialogues; remove Slide 8

--- resources/js/components/story/chapters/ch-7/Slide-2.vue
+++ resources/js/components/story/chapters/ch-7/Slide-2.vue
@@ -8,9 +8,19 @@ import DialogBubble from '@/components/story/ui/DialogBubble.vue';
         <div class="relative flex h-full w-full flex-row justify-around">
             <div class="relative w-1/2"></div>
             <div class="relative w-1/2">
-                <DialogBubble text="Well done coach ! It was a great game." tail-position="right" class="!-left-[40%] !top-[10%]" />
-                <Parent :scale="0.2" :poseNumber="2" :chNumber="7" class="!left-3/4 !top-1/4" />
-                <Parent :scale="0.2" :poseNumber="1" :chNumber="7" class="!left-1/4 !top-1/4" />
+                <DialogBubble text="Well done coach ! It was a great game." tail-position="right" class="!-left-[80%] !top-[10%] xl:!-left-[40%]" />
+                <Parent
+                    :scale="0.2"
+                    :poseNumber="2"
+                    :chNumber="7"
+                    class="!-top-1/2 !left-1/2 md:!-top-1/3 md:!left-1/2 lg:!left-1/2 lg:!top-[20%] lg:!scale-[0.7] xl:!scale-[0.8]"
+                />
+                <Parent
+                    :scale="0.2"
+                    :poseNumber="1"
+                    :chNumber="7"
+                    class="!-top-1/2 !left-0 md:!-top-1/3 md:!left-[10%] lg:!left-[10%] lg:!top-[20%] lg:!scale-[0.7] xl:!scale-[0.8]"
+                />
             </div>
         </div>
     </section>
--- resources/js/components/story/chapters/ch-7/Slide-3.vue
+++ resources/js/components/story/chapters/ch-7/Slide-3.vue
@@ -8,16 +8,30 @@ import DialogBubble from '@/components/story/ui/DialogBubble.vue';
     <section data-auto-animate data-background-image="/assets/story/bg/8.png" class="h-full">
         <div class="relative flex h-full w-full flex-row justify-around">
             <div class="relative w-1/2">
-                <DialogBubble
-                    text="It’s a great time for you to share a moment with the parents and give a feedback."
-                    tail-position="left"
-                    class="!left-1/2"
+                <TheCoach
+                    :scale="0.2"
+                    :poseNumber="3"
+                    :chNumber="1"
+                    class="!-top-1/3 !left-0 md:!-top-1/3 md:!left-[10%] lg:!left-[10%] lg:!top-[20%] lg:!scale-[0.7] xl:!scale-[0.8]"
                 />
-                <TheCoach :scale="0.2" :poseNumber="3" :chNumber="1" class="!left-1/3 !top-1/4 xl:!left-1/2" />
+                <DialogBubble
+                    class="!-top-[10%] !left-[20%] w-max md:!left-[30%] lg:!left-[35%] lg:!top-[20%] xl:!left-[33%] 2xl:!left-[40%]"
+                    text="It’s a great time for you to share a moment with the parents and give a feedback."
+                />
             </div>
             <div class="relative w-1/2">
-                <Parent :scale="0.2" :poseNumber="4" :chNumber="7" class="!left-3/4 !top-1/4" />
-                <Parent :scale="0.2" :poseNumber="3" :chNumber="7" class="!left-1/4 !top-1/4" />
+                <Parent
+                    :scale="0.2"
+                    :poseNumber="4"
+                    :chNumber="7"
+                    class="!-top-1/2 !left-1/2 md:!-top-1/3 md:!left-1/2 lg:!left-1/2 lg:!top-[20%] lg:!scale-[0.7] xl:!scale-[0.8]"
+                />
+                <Parent
+                    :scale="0.2"
+                    :poseNumber="3"
+                    :chNumber="7"
+                    class="!-top-1/2 !left-0 md:!-top-1/3 md:!left-[10%] lg:!left-[10%] lg:!top-[20%] lg:!scale-[0.7] xl:!scale-[0.8]"
+                />
             </div>
         </div>
     </section>
