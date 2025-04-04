a9c396e7cfb76521d3414a724cc451801b305bb7
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-04-04 14:25:06 +0200

    style(story): update layout and text styles for Slide 1 to Slide 4 components

--- resources/js/components/story/chapters/ch-8/Slide-2.vue
+++ resources/js/components/story/chapters/ch-8/Slide-2.vue
@@ -6,16 +6,21 @@ import DialogBubble from '@/components/story/ui/DialogBubble.vue';
 <template>
     <section data-auto-animate data-background-color="#321C16" class="relative h-full">
         <div class="flex h-full flex-col justify-between">
-            <div class="flex flex-col items-start justify-center gap-8 text-white">
-                <h1 class="max-w-sm text-left font-baloo text-6xl font-bold">Hello Coach Simulation</h1>
-                <div data-id="lead" class="text-left font-baloo text-3xl">Great job coach!</div>
+            <div class="flex flex-col items-start justify-center gap-2 font-baloo text-white md:gap-8">
+                <h1 class="max-w-sm text-left text-xl font-bold sm:text-2xl md:text-3xl lg:text-6xl">Hello Coach Simulation</h1>
+                <div data-id="lead" class="text-left text-lg sm:text-xl md:text-2xl lg:text-3xl">Great job coach!</div>
             </div>
             <DialogBubble
                 text="It’s the first step into the world of football coaching."
                 tailPosition="left"
-                class="!left-[40%] !top-[10%] object-contain xl:!left-[35%]"
+                class="!left-[40%] !top-[10%] object-contain 2xl:!left-[40%] 2xl:!top-1/4"
             />
-            <TheCoach :scale="0.2" :poseNumber="2" :chNumber="8" class="!-bottom-2/3 !left-1/3 xl:!left-1/3" />
+            <TheCoach
+                :scale="0.2"
+                :poseNumber="2"
+                :chNumber="8"
+                class="!-bottom-[200%] !left-1/4 md:!-bottom-[170%] md:!left-1/4 2xl:!-bottom-2/3 2xl:!left-1/3"
+            />
         </div>
     </section>
 </template>
--- resources/js/components/story/chapters/ch-8/Slide-4.vue
+++ resources/js/components/story/chapters/ch-8/Slide-4.vue
@@ -5,27 +5,38 @@ import TheCoach from '@/components/story/characters/TheCoach.vue';
 <template>
     <section data-auto-animate data-background-color="#321C16" class="relative h-full">
         <div class="flex h-full flex-row justify-between">
-            <div class="mb-auto flex flex-col items-start justify-center gap-8 text-white">
-                <h1 class="max-w-sm text-left font-baloo text-6xl font-bold">Hello Coach Simulation</h1>
-                <div data-id="lead" class="text-left font-baloo text-3xl">Great job coach!</div>
+            <div class="mb-auto flex flex-col items-start justify-center gap-2 font-baloo text-white md:gap-8">
+                <h1 class="max-w-sm text-left text-xl font-bold sm:text-2xl md:text-3xl lg:text-6xl">Hello Coach Simulation</h1>
+                <div data-id="lead" class="text-left text-lg sm:text-xl md:text-2xl lg:text-3xl">Great job coach!</div>
             </div>
-            <TheCoach :scale="0.2" :poseNumber="4" :chNumber="8" class="!-bottom-2/3 !left-1/3 xl:!left-1/3" />
-            <div class="flex h-fit w-1/2 flex-col gap-6 self-center rounded-md bg-white p-12 font-baloo">
-                <p class="text-3xl font-bold">What’s next ?</p>
+            <TheCoach
+                :scale="0.2"
+                :poseNumber="4"
+                :chNumber="8"
+                class="!-bottom-[170%] !left-1/4 md:!-bottom-[150%] md:!left-1/4 xl:!-bottom-2/3 2xl:!-bottom-2/3 2xl:!left-1/3"
+            />
+            <div class="flex h-fit w-1/2 flex-col gap-3 self-center rounded-md bg-white p-3 font-baloo lg:gap-6 lg:p-12">
+                <p class="text-lg font-bold lg:text-3xl">What’s next ?</p>
                 <div class="flex h-full w-full flex-row items-center justify-between gap-4">
-                    <div class="flex w-1/2 flex-col justify-around gap-4">
-                        <p class="text-xl font-bold">Need help getting started ?</p>
-                        <p class="text-lg">Explore our Help Starter Pack for clear guidance on how to get your coach licence.</p>
+                    <div class="flex w-1/2 flex-col justify-around gap-2 lg:gap-4">
+                        <p class="text-sm font-bold lg:text-xl">Need help getting started ?</p>
+                        <p class="text-xs lg:text-lg">Explore our Help Starter Pack for clear guidance on how to get your coach licence.</p>
                         <!-- TODO: Link to HSP using ziggy route() -->
-                        <a href="/" class="mt-6 w-fit cursor-pointer self-center rounded-md bg-[#321C16] px-4 py-2 text-white"
+                        <a
+                            href="/"
+                            class="mt-6 w-fit cursor-pointer self-center rounded-md bg-[#321C16] px-2 py-1 text-xs text-white lg:px-4 lg:py-2 lg:text-base"
                             >Explore our Help Starter Pack</a
                         >
                     </div>
                     <div class="mx-auto h-52 w-px bg-black"></div>
-                    <div class="flex w-1/2 flex-col gap-4">
-                        <p class="text-xl font-bold">Join the community</p>
-                        <p class="text-lg">Connect with active women coaches, ask your questions, and get inspired inside the Hello Coach Club.</p>
-                        <a :href="route('club.home')" class="mt-6 w-fit cursor-pointer self-center rounded-md bg-[#321C16] px-4 py-2 text-white"
+                    <div class="flex w-1/2 flex-col gap-2 lg:gap-4">
+                        <p class="text-sm font-bold lg:text-xl">Join the community</p>
+                        <p class="text-xs lg:text-lg">
+                            Connect with active women coaches, ask your questions, and get inspired inside the Hello Coach Club.
+                        </p>
+                        <a
+                            :href="route('club.home')"
+                            class="mt-6 w-fit cursor-pointer self-center rounded-md bg-[#321C16] px-2 py-1 text-xs text-white lg:px-4 lg:py-2 lg:text-base"
                             >Join the Hello Coach Club</a
                         >
                     </div>
