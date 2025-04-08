9717ece43692b8ceab85dc0c29fb76e4efb292aa
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-04-08 11:19:30 +0200

    style(feature-card): adjust layout by wrapping icon and title in a flex container

--- resources/js/components/FeatureCard.vue
+++ resources/js/components/FeatureCard.vue
@@ -13,8 +13,10 @@ defineProps<{
     <div
         class="flex aspect-video w-full flex-col justify-between gap-4 rounded-lg bg-secondary/30 p-6 text-left text-secondary-foreground shadow-lg sm:w-1/4"
     >
-        <component :is="icon" class="h-8 w-8 text-accent" />
-        <P class="text-xl font-bold">{{ title }}</P>
+        <div class="flex flex-col">
+            <component :is="icon" class="h-8 w-8 text-accent" />
+            <P class="text-xl font-bold">{{ title }}</P>
+        </div>
         <P class="text-lg">{{ description }}</P>
     </div>
 </template>
