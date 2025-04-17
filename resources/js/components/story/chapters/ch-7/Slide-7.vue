717d85911efb58c851817943e9f06b1bb2ee3ebd
antoineuldry <antoine.uldry@heig-vd.ch>
2025-04-17 10:22:55 +0200

    style(story): adjust positioning of elements in Slide 7 for better layout

--- resources/js/components/story/chapters/ch-7/Slide-7.vue
+++ resources/js/components/story/chapters/ch-7/Slide-7.vue
@@ -12,7 +12,7 @@ const bg = '/assets/story/bg/7-3.png';
             :scale="0.2"
             :poseNumber="5"
             :chNumber="7"
-            class="!-top-[40%] !left-[30%] md:!-top-[30%] md:!left-[30%] xl:!left-[45%] xl:!top-[30%]"
+            class="!-top-[40%] !left-[30%] md:!-top-[30%] md:!left-[30%] xl:!left-[40%] xl:!top-[30%]"
         />
         <Character
             type="parent"
@@ -22,7 +22,7 @@ const bg = '/assets/story/bg/7-3.png';
             class="!-top-[20%] !left-[73%] md:!-top-[15%] md:!left-[75%] xl:!left-[85%] xl:!top-[45%]"
         />
         <DialogBubble
-            class="!left-[45%] !top-[5%] max-w-[15rem] md:!left-[41%] md:!max-w-xs xl:!left-[53%] xl:!top-[15%] 2xl:!left-[52%] 2xl:!top-[30%] 2xl:!max-w-md"
+            class="!left-[45%] !top-[5%] max-w-[15rem] md:!left-[41%] md:!max-w-xs xl:!left-[53%] xl:!top-[15%] 2xl:!left-[50%] 2xl:!top-[25%] 2xl:!max-w-md"
             text="Thanks, coach! It’s great to hear that. By the way, Lily loves to come to training."
             tail-position="right"
         />
